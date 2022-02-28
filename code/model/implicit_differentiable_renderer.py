fix gradient calculations of latent vectors

code/model/implicit_differentiable_renderer.py
@@ -7,10 +7,17 @@ from model.embedder import *
 from model.ray_tracing import RayTracing
 from model.sample_network import SampleNetwork
 
+
+def add_latent(x, latent_code):
+    code_const = torch.broadcast_to(latent_code, (x.shape[0], latent_code.shape[1]))
+    return torch.cat([x, code_const], dim=1)
+
+
 class ImplicitNetwork(nn.Module):
     def __init__(
             self,
             feature_vector_size,
+            latent_code_size,
             d_in,
             d_out,
             dims,
@@ -29,6 +36,9 @@ class ImplicitNetwork(nn.Module):
             embed_fn, input_ch = get_embedder(multires)
             self.embed_fn = embed_fn
             dims[0] = input_ch
+
+        dims[0] += latent_code_size
+
 
         self.num_layers = len(dims)
         self.skip_in = skip_in
@@ -64,10 +74,11 @@ class ImplicitNetwork(nn.Module):
 
         self.softplus = nn.Softplus(beta=100)
 
-    def forward(self, input, compute_grad=False):
+    def forward(self, input, latent_code, compute_grad=False):
         if self.embed_fn is not None:
             input = self.embed_fn(input)
 
+        input = add_latent(input, latent_code)
         x = input
 
         for l in range(0, self.num_layers - 1):
@@ -83,9 +94,9 @@ class ImplicitNetwork(nn.Module):
 
         return x
 
-    def gradient(self, x):
+    def gradient(self, x, latent_code):
         x.requires_grad_(True)
-        y = self.forward(x)[:,:1]
+        y = self.forward(x, latent_code)[:, :1]
         d_output = torch.ones_like(y, requires_grad=False, device=y.device)
         gradients = torch.autograd.grad(
             outputs=y,
@@ -96,10 +107,12 @@ class ImplicitNetwork(nn.Module):
             only_inputs=True)[0]
         return gradients.unsqueeze(1)
 
+
 class RenderingNetwork(nn.Module):
     def __init__(
             self,
             feature_vector_size,
+            latent_vector_size,
             mode,
             d_in,
             d_out,
@@ -110,7 +123,7 @@ class RenderingNetwork(nn.Module):
         super().__init__()
 
         self.mode = mode
-        dims = [d_in + feature_vector_size] + dims + [d_out]
+        dims = [d_in + feature_vector_size + latent_vector_size] + dims + [d_out]
 
         self.embedview_fn = None
         if multires_view > 0:
@@ -132,7 +145,7 @@ class RenderingNetwork(nn.Module):
         self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
 
-    def forward(self, points, normals, view_dirs, feature_vectors):
+    def forward(self, points, normals, view_dirs, feature_vectors, latent_code):
         if self.embedview_fn is not None:
             view_dirs = self.embedview_fn(view_dirs)
 
@@ -143,7 +156,7 @@ class RenderingNetwork(nn.Module):
         elif self.mode == 'no_normal':
             rendering_input = torch.cat([points, view_dirs, feature_vectors], dim=-1)
 
-        x = rendering_input
+        x = add_latent(rendering_input, latent_code)
 
         for l in range(0, self.num_layers - 1):
             lin = getattr(self, "lin" + str(l))
@@ -156,12 +169,20 @@ class RenderingNetwork(nn.Module):
         x = self.tanh(x)
         return x
 
+
 class IDRNetwork(nn.Module):
     def __init__(self, conf):
         super().__init__()
         self.feature_vector_size = conf.get_int('feature_vector_size')
-        self.implicit_network = ImplicitNetwork(self.feature_vector_size, **conf.get_config('implicit_network'))
-        self.rendering_network = RenderingNetwork(self.feature_vector_size, **conf.get_config('rendering_network'))
+
+        implicit_conf = conf.get_config('implicit_network')
+        implicit_conf["latent_code_size"] = conf.get_int('latent_vector_size')
+
+        rendering_conf = conf.get_config('rendering_network')
+        rendering_conf["latent_vector_size"] = conf.get_int('latent_vector_size')
+
+        self.implicit_network = ImplicitNetwork(self.feature_vector_size, **implicit_conf)
+        self.rendering_network = RenderingNetwork(self.feature_vector_size, **rendering_conf)
         self.ray_tracer = RayTracing(**conf.get_config('ray_tracer'))
         self.sample_network = SampleNetwork()
         self.object_bounding_sphere = conf.get_float('ray_tracer.object_bounding_sphere')
@@ -173,6 +194,7 @@ class IDRNetwork(nn.Module):
         uv = input["uv"]
         pose = input["pose"]
         object_mask = input["object_mask"].reshape(-1)
+        latent_code = input["obj"]
 
         ray_dirs, cam_loc = rend_util.get_camera_params(uv, pose, intrinsics)
 
@@ -180,7 +202,11 @@ class IDRNetwork(nn.Module):
 
         self.implicit_network.eval()
         with torch.no_grad():
-            points, network_object_mask, dists = self.ray_tracer(sdf=lambda x: self.implicit_network(x)[:, 0],
+
+            def sdf_fn(x):
+                return self.implicit_network(x, latent_code)[:, 0]
+
+            points, network_object_mask, dists = self.ray_tracer(sdf=sdf_fn,
                                                                  cam_loc=cam_loc,
                                                                  object_mask=object_mask,
                                                                  ray_directions=ray_dirs)
@@ -188,7 +214,7 @@ class IDRNetwork(nn.Module):
 
         points = (cam_loc.unsqueeze(1) + dists.reshape(batch_size, num_pixels, 1) * ray_dirs).reshape(-1, 3)
 
-        sdf_output = self.implicit_network(points)[:, 0:1]
+        sdf_output = self.implicit_network(points, latent_code)[:, 0:1]
         ray_dirs = ray_dirs.reshape(-1, 3)
 
         if self.training:
@@ -210,10 +236,10 @@ class IDRNetwork(nn.Module):
 
             points_all = torch.cat([surface_points, eikonal_points], dim=0)
 
-            output = self.implicit_network(surface_points)
+            output = self.implicit_network(surface_points, latent_code)
             surface_sdf_values = output[:N, 0:1].detach()
 
-            g = self.implicit_network.gradient(points_all)
+            g = self.implicit_network.gradient(points_all, latent_code)
             surface_points_grad = g[:N, 0, :].clone().detach()
             grad_theta = g[N:, 0, :]
 
@@ -233,7 +259,7 @@ class IDRNetwork(nn.Module):
 
         rgb_values = torch.ones_like(points).float().cuda()
         if differentiable_surface_points.shape[0] > 0:
-            rgb_values[surface_mask] = self.get_rbg_value(differentiable_surface_points, view)
+            rgb_values[surface_mask] = self.get_rbg_value(differentiable_surface_points, view, latent_code)
 
         output = {
             'points': points,
@@ -246,12 +272,12 @@ class IDRNetwork(nn.Module):
 
         return output
 
-    def get_rbg_value(self, points, view_dirs):
-        output = self.implicit_network(points)
-        g = self.implicit_network.gradient(points)
+    def get_rbg_value(self, points, view_dirs, latent_code):
+        output = self.implicit_network(points, latent_code)
+        g = self.implicit_network.gradient(points, latent_code)
         normals = g[:, 0, :]
 
         feature_vectors = output[:, 1:]
-        rgb_vals = self.rendering_network(points, normals, view_dirs, feature_vectors)
+        rgb_vals = self.rendering_network(points, normals, view_dirs, feature_vectors, latent_code)
 
         return rgb_vals

code/training/idr_train.py
@@ -1,14 +1,16 @@
+import math
 import os
+import random
 from datetime import datetime
 from pyhocon import ConfigFactory
 import sys
 import torch
-
+import pickle
 import utils.general as utils
 import utils.plots as plt
 
 
-class IDRTrainRunner():
+class IDRTrainRunner:
     def __init__(self, **kwargs):
         torch.set_default_dtype(torch.float32)
         torch.set_num_threads(1)
@@ -87,8 +89,15 @@ class IDRTrainRunner():
                                                                                           **dataset_conf)
         self.n_objs = self.train_dataset.n_objs
 
-        self.latent_vectors = [torch.rand((self.conf.get_int('model.latent_vector_size')), requires_grad=True) for _ in
-                               range(self.n_objs)]
+
+        lat_size = self.conf.get_int('model.latent_vector_size')
+
+        self.lat_vecs = torch.nn.Embedding(self.n_objs, lat_size)
+        torch.nn.init.normal_(
+            self.lat_vecs.weight.data,
+            0.0,
+            1 / math.sqrt(lat_size),
+        )
 
         print('Finish loading data ...')
 
@@ -110,7 +119,20 @@ class IDRTrainRunner():
         self.loss = utils.get_class(self.conf.get_string('train.loss_class'))(**self.conf.get_config('loss'))
 
         self.lr = self.conf.get_float('train.learning_rate')
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+
+        self.optimizer = torch.optim.Adam(
+            [
+                {
+                    "params": self.model.parameters(),
+                    "lr": self.lr
+                },
+                {
+                    "params": self.lat_vecs.parameters(),
+                    "lr": self.lr
+                },
+            ]
+        )
+
         self.sched_milestones = self.conf.get_list('train.sched_milestones', default=[])
         self.sched_factor = self.conf.get_float('train.sched_factor', default=0.0)
         self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, self.sched_milestones,
@@ -128,6 +150,10 @@ class IDRTrainRunner():
         self.start_epoch = 0
         if is_continue:
             old_checkpnts_dir = os.path.join(self.expdir, timestamp, 'checkpoints')
+
+            print("Continuing from", old_checkpnts_dir)
+            with open(os.path.join(old_checkpnts_dir, "latent_table.pickle"), 'rb') as handle:
+                self.lat_vecs = pickle.load(handle)
 
             saved_model_state = torch.load(
                 os.path.join(old_checkpnts_dir, 'ModelParameters', str(kwargs['checkpoint']) + ".pth"))
@@ -166,6 +192,9 @@ class IDRTrainRunner():
                 self.loss.alpha = self.loss.alpha * self.alpha_factor
 
     def save_checkpoints(self, epoch):
+
+        with open(os.path.join(self.checkpoints_path, "latent_table.pickle"), 'wb') as handle:
+            pickle.dump(self.lat_vecs, handle)
         torch.save(
             {"epoch": epoch, "model_state_dict": self.model.state_dict()},
             os.path.join(self.checkpoints_path, self.model_params_subdir, str(epoch) + ".pth"))
@@ -207,15 +236,21 @@ class IDRTrainRunner():
 
         print("training...")
 
+        latent_vec_history = []
+
         for epoch in range(self.start_epoch, self.nepochs + 1):
+            latent_vec_history.append(self.lat_vecs)
+            with open(os.path.join(self.plots_dir, "latent_table.pickle"), 'wb') as handle:
+                pickle.dump(latent_vec_history, handle)
 
             if epoch in self.alpha_milestones:
                 self.loss.alpha = self.loss.alpha * self.alpha_factor
 
-            if epoch % 100 == 0:
+            if epoch % 5 == 0:
                 self.save_checkpoints(epoch)
 
             if epoch % self.plot_freq == 0:
+
                 self.model.eval()
                 if self.train_cameras:
                     self.pose_vecs.eval()
@@ -225,7 +260,7 @@ class IDRTrainRunner():
                 model_input["intrinsics"] = model_input["intrinsics"].cuda()
                 model_input["uv"] = model_input["uv"].cuda()
                 model_input["object_mask"] = model_input["object_mask"].cuda()
-                model_input["obj"] = self.latent_vectors[model_input["obj"]]
+                model_input["obj"] = self.lat_vecs(model_input["obj"]).cuda()
 
                 if self.train_cameras:
                     pose_input = self.pose_vecs(indices.cuda())
@@ -255,7 +290,8 @@ class IDRTrainRunner():
                          self.plots_dir,
                          epoch,
                          self.img_res,
-                         **self.plot_conf
+                         lat_vec=model_input["obj"],
+                         **self.plot_conf,
                          )
 
                 self.model.train()
@@ -264,12 +300,16 @@ class IDRTrainRunner():
 
             self.train_dataset.change_sampling_idx(self.num_pixels)
 
-            for data_index, (indices, model_input, ground_truth) in enumerate(self.train_dataloader):
+            datapoints = list(self.train_dataloader)
+            random.shuffle(datapoints)
+
+
+            for data_index, (indices, model_input, ground_truth) in enumerate(datapoints):
 
                 model_input["intrinsics"] = model_input["intrinsics"].cuda()
                 model_input["uv"] = model_input["uv"].cuda()
                 model_input["object_mask"] = model_input["object_mask"].cuda()
-                model_input["obj"] = self.latent_vectors[model_input["obj"]]
+                model_input["obj"] = self.lat_vecs(model_input["obj"]).cuda()
 
                 if self.train_cameras:
                     pose_input = self.pose_vecs(indices.cuda())
@@ -292,13 +332,13 @@ class IDRTrainRunner():
                 if self.train_cameras:
                     self.optimizer_cam.step()
 
-                print(
-                    '{0} [{1}] ({2}/{3}): loss = {4}, rgb_loss = {5}, eikonal_loss = {6}, mask_loss = {7}, alpha = {8}, lr = {9}'
-                        .format(self.expname, epoch, data_index, self.n_batches, loss.item(),
-                                loss_output['rgb_loss'].item(),
-                                loss_output['eikonal_loss'].item(),
-                                loss_output['mask_loss'].item(),
-                                self.loss.alpha,
-                                self.scheduler.get_lr()[0]))
-
+                if data_index % 50 == 0:
+                    print(
+                        '{0} [{1}] ({2}/{3}): loss = {4}, rgb_loss = {5}, eikonal_loss = {6}, mask_loss = {7}, alpha = {8}, lr = {9}'
+                            .format(self.expname, epoch, data_index, self.n_batches, loss.item(),
+                                    loss_output['rgb_loss'].item(),
+                                    loss_output['eikonal_loss'].item(),
+                                    loss_output['mask_loss'].item(),
+                                    self.loss.alpha,
+                                    self.scheduler.get_lr()[0]))
             self.scheduler.step()

code/utils/plots.py
@@ -8,7 +8,7 @@ import trimesh
 from PIL import Image
 from utils import rend_util
 
-def plot(model, indices, model_outputs ,pose, rgb_gt, path, epoch, img_res, plot_nimgs, max_depth, resolution):
+def plot(model, indices, model_outputs ,pose, rgb_gt, path, epoch, img_res, plot_nimgs, max_depth, resolution, lat_vec):
     # arrange data to plot
     batch_size, num_samples, _ = rgb_gt.shape
 
@@ -32,10 +32,13 @@ def plot(model, indices, model_outputs ,pose, rgb_gt, path, epoch, img_res, plot
 
     data = []
 
+    def sdf_plot(x):
+        return model.implicit_network(x, lat_vec)[:, 0]
+
     # plot surface
     surface_traces = get_surface_trace(path=path,
                                        epoch=epoch,
-                                       sdf=lambda x: model.implicit_network(x)[:, 0],
+                                       sdf=sdf_plot,
                                        resolution=resolution
                                        )
     try:
@@ -52,7 +55,7 @@ def plot(model, indices, model_outputs ,pose, rgb_gt, path, epoch, img_res, plot
         sampling_idx = torch.randperm(p.shape[0])[:2048]
         p = p[sampling_idx, :]
 
-        val = model.implicit_network(p)
+        val = model.implicit_network(p, lat_vec)
         caption = ["sdf: {0} ".format(v[0].item()) for v in val]
 
         data.append(get_3D_scatter_trace(p, name='intersection_points_{0}'.format(i), caption=caption))